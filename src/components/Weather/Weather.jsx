f74f3d217618330ec10c26c9192bf84a8c4eaea1
ArchibaldKronin <mr_froll@mail.ru>
2022-10-17 23:04:34 +0300

    Карточки погоды и полоса прокрутки

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { WeatherCard } from "../WeatherCard/WeatherCard";
 import styles from './Weather.module.css';
 
 export const Weather = ({ data }) => {
@@ -8,11 +9,18 @@ export const Weather = ({ data }) => {
             {data.map(interval => {
                 return (
                     <div key={interval.startTime}>
-                        <h3>{interval.startTime}</h3>
+                        {/* <h3>{interval.startTime}</h3>
                         <h4>{interval.weatherCode}</h4>
                         <p>{`Облачность: ${interval.weatherValues.cloudCover}%`}</p>
                         <p>{`Температура: ${interval.weatherValues.temperature} градусов Цельсия`}</p>
-                        <p>{`Скорость ветра: ${interval.weatherValues.windSpeed} м/с`}</p>
+                        <p>{`Скорость ветра: ${interval.weatherValues.windSpeed} м/с`}</p> */}
+                        <WeatherCard startTime={interval.startTime} weatherValues={
+                            {
+                                cloudCover: interval.weatherValues.cloudCover,
+                                temperature: interval.weatherValues.temperature,
+                                windSpeed: interval.weatherValues.windSpeed
+                            }}
+                            weatherCode={interval.weatherCode} />
                     </div>
                 )
 
--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import styles from './WeatherCard.module.css';
 
-export const WeatherCard = ({data}) => {
-  return (
-    <div>WeatherCard</div>
-  )
+export const WeatherCard = ({ weatherCode, startTime, weatherValues }) => {
+    return (
+        <div className={styles.weatherCardContainer}>
+            <div className={styles.icon}>
+                {weatherCode}
+            </div>
+            <div className={styles.weatherCard}>
+                <h3 className={styles.time}>
+                    {startTime}
+                </h3>
+                <div className={styles.weatherParamsContainer}>
+                    Температура: <span className={styles.weatherParams}>{weatherValues.temperature} С</span>
+                </div>
+                <div className={styles.weatherParamsContainer}>
+                    Облачность: <span className={styles.weatherParams}>{weatherValues.cloudCover} %</span>
+                </div>
+                <div className={styles.weatherParamsContainer}>
+                    Скорость ветра: <span className={styles.weatherParams}>{weatherValues.windSpeed} м/с</span>
+                </div>
+            </div>
+        </div>
+    )
 }
